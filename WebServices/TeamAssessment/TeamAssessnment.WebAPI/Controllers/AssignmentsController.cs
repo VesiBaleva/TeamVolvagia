a8e3f515cf956b9f517aca39f4b06a6068b73f1d
Veselina Baleva <bveselina@gmail.com>
2013-10-16 20:04:20 +0300

    added AssignmentsController

---
+++ WebServices/TeamAssessment/TeamAssessnment.WebAPI/Controllers/AssignmentsController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using TeamAssessment.Models;
+using TeamAssessnment.Data;
+using TeamAssessnment.WebAPI.Models;
+
+namespace TeamAssessnment.WebAPI.Controllers
+{
+    public class AssignmentsController : BaseApiController
+    {
+        [HttpGet]
+        public IQueryable<AssignmentsModel> GetAll()
+        {
+            var responseMsg = this.PerformOperationAndHandleExceptions(() =>
+            {
+                var sessionKey = this.GetHeaderValue(Request.Headers, "sessionKey");
+                var context = new TeamAssessnmentContext();
+
+                var user = context.Users.FirstOrDefault(usr => usr.SessionKey == sessionKey);
+                if (user == null)
+                {
+                    throw new InvalidOperationException("Invalid username or password");
+                }
+
+                var assignmentEntities = context.Assignments;
+                var models =
+                    (from assignmentEntity in assignmentEntities
+                     where assignmentEntity.User.Id == user.Id
+                     select new AssignmentsModel()
+                     {
+                          Name = assignmentEntity.Name,
+                          MaxValue = assignmentEntity.MaxValue
+                     });
+                return models;
+            });
+
+
+            return responseMsg;
+        }
+
+        [HttpGet]
+        public IQueryable<AssignmentsModel> GetById(int categoryId)
+        {
+            var responseMsg = this.PerformOperationAndHandleExceptions(() =>
+            {
+                var sessionKey = this.GetHeaderValue(Request.Headers, "sessionKey");
+                var context = new TeamAssessnmentContext();
+
+                var user = context.Users.FirstOrDefault(usr => usr.SessionKey == sessionKey);
+                if (user == null)
+                {
+                    throw new InvalidOperationException("Invalid username or password");
+                }
+
+                var assignmentEntities = context.Assignments;
+                var models =
+                    (from assignmentEntity in assignmentEntities
+                     where assignmentEntity.User.Id == user.Id && assignmentEntity.Category.Id == categoryId
+                     select new AssignmentsModel()
+                     {
+                         Name = assignmentEntity.Name,
+                         MaxValue = assignmentEntity.MaxValue
+                     });
+                return models;
+            });
+
+            return responseMsg;
+        }
+    }
+}
